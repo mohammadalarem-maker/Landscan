049869cc473c23a47f64b88a5007cd0dbd9b4b0a
mohammadalarem-maker <mohammadalarem@email.com>
2026-06-14 21:10:25 +0300

    إصلاح توافقية Compose Compiler مع كوتلن 1.9.24

--- AreaSurvey/app/build.gradle.kts
+++ AreaSurvey/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -34,6 +33,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
